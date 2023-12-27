675a856710ab306ea7afeb2ee9bc7ae1310ff8c5
kietpt2003 <tuankiet29012003@gmail.com>
2023-12-27 17:22:46 +0700

    write UserManage.js get all users

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManagement.scss';
+import { handleGetAllUsers } from '../../services/userService';
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrayUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await handleGetAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrayUsers: response.users
+            })
+        }
     }
-
 
     render() {
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-center'>
+                    Manage User with Kiet
+                </div>
+                <div className='mt-4 mx-1'>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Email</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Address</th>
+                                <th>Action</th>
+                            </tr>
+                            {this.state.arrayUsers.map((user) => (
+                                <tr className="tableRow" key={user.id}>
+                                    <td>
+                                        {user.email}
+                                    </td>
+                                    <td>
+                                        {user.firstName}
+                                    </td>
+                                    <td>
+                                        {user.lastName}
+                                    </td>
+                                    <td>
+                                        {user.address}
+                                    </td>
+                                    <td>
+                                        <button className="btn-edit"><i class="fas fa-pencil-alt"></i></button>
+                                        <button className="btn-delete"><i class="fas fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,11 @@ const handleLoginService = (email, password) => {
     return axios.post('/api/login', { email, password })
 }
 
+const handleGetAllUsers = (id) => {
+    return axios.get(`/api/get-all-users?id=${id}`)
+}
+
 export {
-    handleLoginService
+    handleLoginService,
+    handleGetAllUsers
 }
