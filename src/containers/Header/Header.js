decbb55a4f23995c27c29d4779bee98fecd627d4
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-01 21:38:01 +0700

    create change language function

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -5,8 +5,13 @@ import * as actions from "../../store/actions";
 import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
+import { LANGUAGES } from '../../utils';
 
 class Header extends Component {
+
+    handleChangeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language);
+    }
 
     render() {
         const { processLogout } = this.props;
@@ -18,10 +23,15 @@ class Header extends Component {
                     <Navigator menus={adminMenu} />
                 </div>
 
-                {/* nút logout */}
-                <div className="btn btn-logout" onClick={processLogout}>
-                    <i className="fas fa-sign-out-alt"></i>
+                <div className='languages'>
+                    <span className={`${this.props.language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}`} onClick={() => { this.handleChangeLanguage(LANGUAGES.VI) }}>VN</span>
+                    <span className={`${this.props.language === LANGUAGES.EN ? 'language-en active' : 'language-en'}`} onClick={() => { this.handleChangeLanguage(LANGUAGES.EN) }}>EN</span>
+                    {/* nút logout */}
+                    <div className="btn btn-logout" onClick={processLogout}>
+                        <i className="fas fa-sign-out-alt" title='logout'></i>
+                    </div>
                 </div>
+
             </div>
         );
     }
@@ -30,13 +40,15 @@ class Header extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
         processLogout: () => dispatch(actions.processLogout()),
+        changeLanguageAppRedux: (language) => dispatch(actions.changeLanguageApp(language))
     };
 };
 
