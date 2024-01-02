fc4a1520e82fbc3cf9dc83de94057a427b986a2a
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-02 19:25:22 +0700

    fix bug about.js

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -14,9 +14,9 @@ class About extends Component {
                         <iframe width="100%" height="315"
                             src="https://www.youtube.com/embed/147SkAVXEqM?si=o7OspVooFGWqrZiX"
                             title="YouTube video player"
-                            frameborder="0"
+                            frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowfullscreen>
+                            allowFullScreen>
                         </iframe>
                     </div>
                     <div className='content-right'>
